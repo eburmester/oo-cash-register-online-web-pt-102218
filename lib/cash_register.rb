8002d5d16a5e07563c04d0e7ab98312f6fbaa74e
Elyse Burmester <burmesterelyse@gmail.com>
2018-11-16 14:55:47 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,12 @@ class CashRegister
     if quantity > 1 
       counter = 0 
         while counter < quantity 
-        
+        @items << item
+        counter += 1
+      end
+    else
+      @items << item
+    end
   end
   
 end
