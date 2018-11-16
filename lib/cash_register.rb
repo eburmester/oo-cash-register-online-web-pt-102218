da79f7c2d741e1678b08c75e00505445a4b1e325
Elyse Burmester <burmesterelyse@gmail.com>
2018-11-16 14:50:23 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,11 @@
 class CashRegister
   attr_accessor :total, :price, :discount, :items 
   
-  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
   
   
 end
