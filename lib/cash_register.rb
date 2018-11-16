372ea1437d21efe3e228e41d60a8b253855b2735
Elyse Burmester <burmesterelyse@gmail.com>
2018-11-16 14:54:25 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,9 @@ class CashRegister
   def add_item(item, price, quantity = 1)
     @price = price
     @total += @price * quantity
-    if 
+    if quantity > 1 
+      counter = 0 
+        while  
   end
   
 end
